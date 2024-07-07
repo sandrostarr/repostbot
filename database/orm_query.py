ab0f33d3b06aa0613fb1e9ecd2ff3a451093f251
0x0nion <Dropup@protonmail.com>
2024-07-08 06:58:25 +0700

    #2 Написал каркас бота, нужно создавать БД и тестировать пагинацию. Сделал парочку доп функций в базе для парсинга ДБ, можно поправить везде добавил TODO, сейчас все работает но краш на кнопке так как надо тянуть с БД данные

--- database/orm_query.py
+++ database/orm_query.py
@@ -1,9 +1,42 @@
+import math
 from typing import Type
 
 from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from aiogram.types import Message
 from database.models import UserInfo
+
+
+#TODO: класс для пагинации и работы с БД взял с урока надо посчмотреть и может адаптировать
+class Paginator:
+    def __init__(self, array: list | tuple, page: int = 1, per_page: int=1):
+        self.array = array
+        self.per_page = per_page
+        self.page = page
+        self.len = len(self.array)
+
+        self.pages = math.ceil(self.len / self.per_page)
+
+    def __get_slice(self):
+        start = (self.page - 1) * self.per_page
+        stop = start * self.per_page
+        return self.array[start:stop]
+
+    def get_page(self):
+        page_item = self.__get_slice()
+        return page_item
+
+    def has_next(self):
+        if self.page < self.pages:
+            return self.page + 1
+        return False
+
+
+
+
+
+
+
 
 #TODO: запрос FID нужно убрать и вытсавить наальные значения в таблицк как null
 async def orm_add_user_info(session: AsyncSession, msg: Message):
